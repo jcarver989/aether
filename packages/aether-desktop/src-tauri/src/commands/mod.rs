ea8b5890300257c3b8303f9d66767ae9b9c5b759
Josh Carver <jcarver989@gmail.com>
2025-09-15 11:12:07 -0700

    chore: remove unused desktop app

--- packages/aether-desktop/src-tauri/src/commands/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod chat;
-pub mod config;
-
-pub use chat::*;
-pub use config::*;
--- packages/aether-desktop/src/hooks/useSelector.ts
+++
@@ -1,8 +0,0 @@
-import { AppState } from "@/state/store";
-import { useAppContext } from "./useAppContext";
-import { useStore } from "zustand";
-
-export function useSelector<T>(selector: (state: AppState) => T): T {
-  const { store } = useAppContext();
-  return useStore(store, selector);
-}
