f5151ccada0178d98968d071659067d12c2e3a36
Josh Carver <jcarver989@gmail.com>
2025-09-18 11:07:32 -0700

    checkpoint

--- packages/aether/src/agent/agent.rs
+++ packages/aether/src/agent/agent.rs
@@ -15,25 +15,29 @@ use futures::Stream;
 use futures::StreamExt;
 use futures::pin_mut;
 use std::sync::Arc;
+use tokio::sync::mpsc::Receiver;
+use tokio::sync::mpsc::Sender;
 use tokio::sync::mpsc::error::SendError;
 use tokio::sync::{Mutex, mpsc};
 use tokio::task::JoinHandle;
 
 pub struct Agent<T: ModelProvider> {
-    llm: T,
+    llm: Arc<T>,
     mcp: Arc<Mutex<McpManager>>,
-    context: Context,
+    context: Arc<Mutex<Context>>,
 }
 
 impl<T: ModelProvider + 'static> Agent<T> {
-    pub fn new(llm: T, mcp_client: McpManager, messages: Vec<ChatMessage>) -> Self {
+    pub fn new(llm: T, mcp_manager: McpManager, messages: Vec<ChatMessage>) -> Self {
+        let mcp = Arc::new(Mutex::new(mcp_manager));
+
         Self {
-            llm: llm,
-            mcp: Arc::new(Mutex::new(mcp_client)),
-            context: Context::new(
+            llm: Arc::new(llm),
+            mcp,
+            context: Arc::new(Mutex::new(Context::new(
                 messages,
                 Vec::new(), // populated when tools are discovered
-            ),
+            ))),
         }
     }
 
@@ -44,7 +48,7 @@ impl<T: ModelProvider + 'static> Agent<T> {
     pub async fn send(&mut self, message: UserMessage) -> impl Stream<Item = AgentMessage> {
         match message {
             UserMessage::Text { content } => {
-                self.context.add_message(ChatMessage::User {
+                self.context.lock().await.add_message(ChatMessage::User {
                     content,
                     timestamp: IsoString::now(),
                 });
@@ -74,19 +78,28 @@ impl<T: ModelProvider + 'static> Agent<T> {
             })
             .collect();
 
-        self.context.set_tools(tools);
+        self.context.lock().await.set_tools(tools);
         Ok(())
     }
 
     async fn process_user_message(&mut self) -> impl Stream<Item = AgentMessage> {
-        stream! {
-            match self.update_tools().await {
-                Ok(_) => {}
-                Err(e) => {
-                    yield AgentMessage::Error { message: format!("Error fetching tools: {:?}", e) };
-                    return;
-                }
-            };
+        let (tool_executor_handle, tool_call_tx, mut tool_result_rx) =
+            ToolExecutor::start(self.mcp.clone());
+
+        let (tx, rx) = mpsc::channel::<AgentMessage>(100);
+        let stream = tokio_stream::wrappers::ReceiverStream::new(rx);
+        let llm = self.llm.clone();
+        let context = self.context.clone();
+        let mcp = self.mcp.clone();
+
+        let task = tokio::spawn(async move {
+            let update_tools_result = self.update_tools().await;
+
+            if let Err(e) = update_tools_result {
+                tx.send(AgentMessage::Error {
+                    message: format!("Error fetching tools: {:?}", e),
+                });
+            }
 
             // Main "agentic" loop.
             // Each iteration of the outer loop procesess 1 LLM call
@@ -95,61 +108,28 @@ impl<T: ModelProvider + 'static> Agent<T> {
             let mut current_iteration = 1;
 
             loop {
-                tracing::debug!("Starting agent loop iteration {}", current_iteration);
-
-                if current_iteration >= max_iterations {
-                    tracing::error!("Max iterations reached: {}", max_iterations);
-                    yield AgentMessage::Error { message: "Max iterations reached".to_string() };
-                    return;
-                }
-
-                tracing::debug!("Getting LLM response stream for iteration {}", current_iteration);
                 let model_name = self.llm.display_name();
+                let (llm_tx, llm_rx) = mpsc::channel::<AgentMessage>(100);
 
-                let mut tool_call_manager = ToolCallManager::new();
-                let llm_stream =  self.process_llm_stream().await;
-                pin_mut!(llm_stream);
+                let llm_future = Self::process_llm_stream(&llm, context, llm_tx, tool_call_tx);
 
-                while let Some(event) = llm_stream.next().await {
-                    match &event {
-                        AgentMessage::Text { chunk, is_complete: true, .. } => {
-                            self.context.add_message(ChatMessage::Assistant {
-                                content: chunk.clone(),
-                                timestamp: IsoString::now(),
-                                tool_calls: tool_call_manager.requests.clone()
-                            });
-                        },
-
-                        AgentMessage::ToolCall { tool_call_id, name, arguments: Some(args), is_complete: true, .. } => {
-                            tool_call_manager.execute_tool(self.mcp.clone(),
-                            ToolCallRequest { id: tool_call_id.clone(), name: name.clone(), arguments: args.clone() });
-                        },
-
-                        _ => {}
-                    }
-
-
-                    yield event;
-                }
-
-
-                if tool_call_manager.tool_handles.is_empty() {
-                    tracing::debug!("No tool requests, terminating agent loop");
-                    return;
-                }
-
-                if !tool_call_manager.tool_handles.is_empty() {
-                    tracing::debug!("Waiting for {} tool results after stream completion...", tool_call_manager.tool_handles.len());
-                    let tool_results = tool_call_manager.wait_for_all_tools_to_execute().await;
+             
 
                     for result in tool_results {
-                        tracing::debug!("Tool result received: {} -> {}", result.name, result.result.len());
+                        tracing::debug!(
+                            "Tool result received: {} -> {}",
+                            result.name,
+                            result.result.len()
+                        );
 
-                        self.context.add_message(ChatMessage::ToolCallResult {
-                            tool_call_id: result.id.clone(),
-                            content: result.result.clone(),
-                            timestamp: IsoString::now()
-                        });
+                        self.context
+                            .lock()
+                            .await
+                            .add_message(ChatMessage::ToolCallResult {
+                                tool_call_id: result.id.clone(),
+                                content: result.result.clone(),
+                                timestamp: IsoString::now(),
+                            });
 
                         yield AgentMessage::ToolCall {
                             tool_call_id: result.id.clone(),
@@ -164,109 +144,171 @@ impl<T: ModelProvider + 'static> Agent<T> {
 
                 current_iteration += 1;
             }
-        }
+        });
+        return stream;
     }
 
-    async fn x() {}
+    async fn process_llm_stream(
+        llm: &T,
+        context: Arc<Mutex<Context>>,
+        tx: Sender<AgentMessage>,
+        tool_call_tx: Sender<ToolCallRequest>,
+    ) -> () {
+        let c = context.lock().await;
+        let response_stream = llm.stream_response(&c);
+        let model_name = llm.display_name();
+        pin_mut!(response_stream);
 
-    async fn process_llm_stream(&self) -> impl Stream<Item = AgentMessage> {
-        stream! {
-            let response_stream = self.llm.stream_response(&self.context);
-            let model_name = self.llm.display_name();
-            pin_mut!(response_stream);
+        let mut current_message_id: Option<String> = None;
+        let mut message_content = String::new();
+        let mut tool_call_requests = Vec::<ToolCallRequest>::new();
 
-            let mut current_message_id: Option<String> = None;
-            let mut message_content = String::new();
+        while let Some(event) = response_stream.next().await {
+            use LlmResponse::*;
+            match event {
+                Ok(Start { message_id }) => {
+                    current_message_id = Some(message_id);
+                }
 
-            while let Some(event) = response_stream.next().await {
-                use LlmResponse::*;
-                match event {
-                    Ok(Start { message_id }) => {
-                        current_message_id = Some(message_id);
+                Ok(Text { chunk }) => {
+                    message_content.push_str(&chunk);
+                    if let Some(ref id) = current_message_id {
+                        tx.send(AgentMessage::Text {
+                            message_id: id.clone(),
+                            chunk,
+                            is_complete: false,
+                            model_name: model_name.clone(),
+                        });
                     }
+                }
 
-                    Ok(Text { chunk }) => {
-                        message_content.push_str(&chunk);
-                        if let Some(ref id) = current_message_id {
-                            yield AgentMessage::Text {
-                                message_id: id.clone(),
-                                chunk,
-                                is_complete: false,
-                                model_name: model_name.clone(),
-                            };
+                Ok(ToolRequestStart { id, name }) => {
+                    tx.send(AgentMessage::ToolCall {
+                        tool_call_id: id,
+                        name,
+                        arguments: None,
+                        result: None,
+                        is_complete: false,
+                        model_name: model_name.clone(),
+                    });
+                }
+
+                Ok(ToolRequestArg { id, chunk }) => {
+                    tx.send(AgentMessage::ToolCall {
+                        tool_call_id: id,
+                        name: String::new(),
+                        arguments: Some(chunk.to_string()),
+                        result: None,
+                        is_complete: false,
+                        model_name: model_name.clone(),
+                    });
+                }
+
+                Ok(ToolRequestComplete { tool_call }) => {
+                    tracing::debug!(
+                        "Tool request completed: {} ({})",
+                        tool_call.name,
+                        tool_call.id
+                    );
+
+                    tool_call_requests.push(tool_call.clone());
+                    tool_call_tx.send(tool_call.clone());
+
+                    tx.send(AgentMessage::ToolCall {
+                        tool_call_id: tool_call.id,
+                        name: String::new(),
+                        arguments: Some(tool_call.arguments),
+                        result: None,
+                        is_complete: false,
+                        model_name: model_name.clone(),
+                    });
+                }
+
+                Ok(Done) => {
+                    break;
+                }
+
+                Ok(Error { message }) => {
+                    tx.send(AgentMessage::Error {
+                        message: message.to_string(),
+                    });
+                    return;
+                }
+
+                Err(e) => {
+                    tx.send(AgentMessage::Error {
+                        message: e.to_string(),
+                    });
+                    return;
+                }
+            }
+        }
+
+        if let Some(ref id) = current_message_id {
+            context.lock().await.add_message(ChatMessage::Assistant {
+                content: message_content.clone(),
+                timestamp: IsoString::now(),
+                tool_calls: tool_call_requests,
+            });
+
+            tx.send(AgentMessage::Text {
+                message_id: id.clone(),
+                chunk: message_content,
+                is_complete: true,
+                model_name: model_name.clone(),
+            });
+        }
+    }
+}
+
+struct ToolExecutor {}
+
+impl ToolExecutor {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn start(
+        mcp: Arc<Mutex<McpManager>>,
+    ) -> (
+        JoinHandle<()>,
+        Sender<ToolCallRequest>,
+        Receiver<ToolCallResult>,
+    ) {
+        let (tool_call_tx, mut tool_call_rx) = mpsc::channel::<ToolCallRequest>(100);
+        let (tool_result_tx, tool_result_rx) = mpsc::channel::<ToolCallResult>(100);
+
+        let handle = tokio::spawn(async move {
+            while let Some(request) = tool_call_rx.recv().await {
+                let result_str = match serde_json::from_str(&request.arguments) {
+                    Ok(args) => {
+                        let mcp_client_guard = mcp.lock().await;
+                        match mcp_client_guard.execute_tool(&request.name, args).await {
+                            Ok(result) => result.to_string(),
+                            Err(e) => {
+                                format!("Tool execution failed: {}", e)
+                            }
                         }
-                    }
-
-                    Ok(ToolRequestStart { id, name }) => {
-                        yield AgentMessage::ToolCall {
-                            tool_call_id: id,
-                            name,
-                            arguments: None,
-                            result: None,
-                            is_complete: false,
-                            model_name: model_name.clone(),
-                        };
-                    }
-
-                    Ok(ToolRequestArg { id, chunk }) => {
-                        yield AgentMessage::ToolCall {
-                            tool_call_id: id,
-                            name: String::new(),
-                            arguments: Some(chunk.to_string()),
-                            result: None,
-                            is_complete: false,
-                            model_name: model_name.clone(),
-                        };
-                    }
-
-                    Ok(ToolRequestComplete { tool_call }) => {
-                        tracing::debug!(
-                            "Tool request completed: {} ({})",
-                            tool_call.name,
-                            tool_call.id
-                        );
-
-                        yield AgentMessage::ToolCall {
-                            tool_call_id: tool_call.id,
-                            name: String::new(),
-                            arguments: Some(tool_call.arguments),
-                            result: None,
-                            is_complete: false,
-                            model_name: model_name.clone(),
-                        }
-
-                        //tool_call_manager.execute_tool(self.mcp.clone(), tool_call.clone());
-                    }
-
-                    Ok(Done) => {
-                        break;
-                    }
-
-                    Ok(Error { message }) => {
-                        yield AgentMessage::Error {
-                            message: message.to_string(),
-                        };
-                        return;
                     }
 
                     Err(e) => {
-                        yield AgentMessage::Error {
-                            message: e.to_string(),
-                        };
-                        return;
+                        tracing::error!("Invalid tool arguments for {}: {}", request.name, e);
+                        format!("Invalid tool arguments: {}", e)
                     }
-                }
-            }
-
-            if let Some(ref id) = current_message_id {
-                yield AgentMessage::Text {
-                    message_id: id.clone(),
-                    chunk: message_content,
-                    is_complete: true,
-                    model_name: model_name.clone()
                 };
+
+                tool_result_tx
+                    .send(ToolCallResult {
+                        id: request.id.clone(),
+                        name: request.name.clone(),
+                        arguments: request.arguments,
+                        result: result_str,
+                    })
+                    .await;
             }
-        }
+        });
+
+        (handle, tool_call_tx, tool_result_rx)
     }
 }
 
