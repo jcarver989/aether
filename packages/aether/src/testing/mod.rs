f5758364199b827f46303a9bb7e9be2ca53b7971
Josh Carver <jcarver989@gmail.com>
2025-10-10 22:18:23 -0600

    chore(aether): Add agent_tests.rs as cannoical testing example

--- packages/aether/src/testing/mod.rs
+++ packages/aether/src/testing/mod.rs
@@ -1,5 +1,9 @@
+pub mod agent_message;
 pub mod fake_llm;
+pub mod fake_mcp;
 pub mod fs;
+pub mod llm_response;
 
 pub use fake_llm::FakeLlmProvider;
+pub use fake_mcp::FakeMcpServer;
 pub use fs::*;
