b195c4658442296af3a81ed004235672a1885d84
Josh Carver <jcarver989@gmail.com>
2025-10-11 23:06:42 -0600

    refactor(aether): Cleanup agent_tests and run_agent helper

--- packages/aether/src/testing/utils.rs
+++ packages/aether/src/testing/utils.rs
@@ -9,18 +9,7 @@ use crate::types::LlmResponse;
 
 use super::FakeLlmProvider;
 
-/// run an agent with expected messages and assert the output.
 pub async fn run_agent(
-    llm_responses: &[Vec<LlmResponse>],
-    user_messages: &[UserMessage],
-    expected_agent_messages: Vec<AgentMessage>,
-) -> Result<(), Box<dyn Error>> {
-    let messages = collect_agent_messages(llm_responses, user_messages).await?;
-    assert_eq!(messages, expected_agent_messages);
-    Ok(())
-}
-
-pub async fn collect_agent_messages(
     llm_responses: &[Vec<LlmResponse>],
     user_messages: &[UserMessage],
 ) -> Result<Vec<AgentMessage>, Box<dyn Error>> {
