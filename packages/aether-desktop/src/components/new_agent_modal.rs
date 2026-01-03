f469a0f0f72f04d4297f8332d156945044eeb506
Josh Carver <jcarver989@gmail.com>
2026-01-02 21:27:43 -0800

    fix: textbox value bug

--- packages/aether-desktop/src/components/new_agent_modal.rs
+++ packages/aether-desktop/src/components/new_agent_modal.rs
@@ -24,15 +24,9 @@ pub fn NewAgentForm(
 ) -> Element {
     let settings: Signal<Settings> = use_context();
 
-    // Get the first server name as default, or use custom
-    let default_server = {
+    let mut selected_server = use_signal(|| {
         let s = settings.read();
         s.agent_servers.keys().next().cloned()
-    };
-
-    let mut selected_server = use_signal(|| {
-        default_server
-            .clone()
             .unwrap_or_else(|| CUSTOM_SERVER.to_string())
     });
     let custom_command_line = use_signal(|| AgentConfig::default().command_line);
