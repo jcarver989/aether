2061eb53bcc581d0f8bb6333698b38d20b2bdaa0
Josh Carver <jcarver989@gmail.com>
2026-02-16 22:42:47 -0800

    cleanups

--- packages/wisp/src/tui/mod.rs
+++ packages/wisp/src/tui/mod.rs
@@ -1,6 +1,7 @@
 pub mod combobox;
 pub mod component;
 pub mod renderer;
+pub mod runtime;
 pub mod screen;
 pub mod soft_wrap;
 pub mod spinner;
@@ -9,4 +10,5 @@ pub mod theme;
 pub use combobox::{Combobox, Searchable};
 pub use component::{Component, HandlesInput, InputOutcome, RenderContext};
 pub use renderer::{Cursor, CursorComponent, RenderOutput, Renderer};
+pub use runtime::spawn_terminal_event_task;
 pub use screen::Line;
